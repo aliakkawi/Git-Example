25a00dc794b3cc5905a1b13275b71a59470fc0a0
Ali Akkawi <ali_akkawi1@yahoo.com>
2018-10-08 18:20:40 +0300

    added player 2 from child branch

--- Git Example/GameScene.swift
+++ Git Example/GameScene.swift
@@ -22,7 +22,7 @@ class GameScene: SKScene {
         
         
         let player2 = SKSpriteNode(imageNamed: "player2")
-        player2.setScale(0.5)
+        player2.setScale(0.7)
         player2.position = CGPoint(x: 0, y: 100)
         player2.zPosition = 1.0
         addChild(player2)
